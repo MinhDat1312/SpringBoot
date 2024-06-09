ba6004b136a7715d4fb2523f1c570722c518021c
MinhDat1312 <nguyenthangdat84@gmail.com>
2024-06-09 19:53:19 +0700

    security

--- src/main/java/vn/hoidanit/jobhunter/controller/HelloController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/HelloController.java
@@ -1,11 +1,8 @@
 package vn.hoidanit.jobhunter.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import vn.hoidanit.jobhunter.domain.RestResponse;
 import vn.hoidanit.jobhunter.service.exception.IdInvalidException;
 
 @RestController
