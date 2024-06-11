5b4bcd5688eeff86d4ffd0007de6613d2fcdf78e
MinhDat1312 <nguyenthangdat84@gmail.com>
2024-06-11 10:05:07 +0700

    update jwt

--- src/main/java/vn/hoidanit/jobhunter/config/CustomAuthenticationEntryPoint.java
+++ src/main/java/vn/hoidanit/jobhunter/config/CustomAuthenticationEntryPoint.java
@@ -1,6 +1,7 @@
 package vn.hoidanit.jobhunter.config;
 
 import java.io.IOException;
+import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
@@ -32,7 +33,11 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
 
         RestResponse<Object> res = new RestResponse<>();
         res.setStatusCode(HttpStatus.UNAUTHORIZED.value());
-        res.setError(authException.getCause().getMessage());
+
+        String errorMessage = Optional.ofNullable(authException.getCause()).map(Throwable::getMessage)
+                .orElse(authException.getMessage());
+        res.setError(errorMessage);
+
         res.setMessage("Token không hợp lệ (hết hạn, không đúng)");
 
         mapper.writeValue(response.getWriter(), res);
