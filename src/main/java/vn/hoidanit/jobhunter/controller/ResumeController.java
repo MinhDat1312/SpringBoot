ce22e9e3408be56b448a89074f321d2bc543f048
MinhDat1312 <nguyenthangdat84@gmail.com>
2024-06-23 23:29:19 +0700

    crud resume

---
+++ src/main/java/vn/hoidanit/jobhunter/controller/ResumeController.java
@@ -0,0 +1,96 @@
+package vn.hoidanit.jobhunter.controller;
+
+import java.util.Optional;
+import java.util.regex.Pattern;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.turkraft.springfilter.boot.Filter;
+
+import jakarta.validation.Valid;
+import vn.hoidanit.jobhunter.domain.Resume;
+import vn.hoidanit.jobhunter.domain.response.ResultPaginationDTO;
+import vn.hoidanit.jobhunter.domain.response.resume.ResCreateResumeDTO;
+import vn.hoidanit.jobhunter.domain.response.resume.ResResume;
+import vn.hoidanit.jobhunter.domain.response.resume.ResUpdateResumeDTO;
+import vn.hoidanit.jobhunter.service.ResumeService;
+import vn.hoidanit.jobhunter.util.exception.IdInvalidException;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping("/api/v1")
+public class ResumeController {
+    private ResumeService resumeService;
+
+    public ResumeController(ResumeService resumeService) {
+        this.resumeService = resumeService;
+    }
+
+    @PostMapping("/resumes")
+    public ResponseEntity<ResCreateResumeDTO> createResume(@Valid @RequestBody Resume resume)
+            throws IdInvalidException {
+        if (this.resumeService.checkResumeExistByUserAndJob(resume) == false) {
+            throw new IdInvalidException("User id or Job id doesn't exist");
+        } else {
+            return ResponseEntity.status(HttpStatus.CREATED).body(this.resumeService.handleCreateResume(resume));
+        }
+    }
+
+    @PutMapping("/resumes")
+    public ResponseEntity<ResUpdateResumeDTO> updateResume(@RequestBody Resume resume) throws IdInvalidException {
+        Resume currentResume = this.resumeService.handleGetResumeById(resume.getId());
+
+        if (currentResume != null) {
+            currentResume.setStatus(resume.getStatus());
+            return ResponseEntity.ok().body(this.resumeService.handleUpdateResume(currentResume));
+        } else {
+            throw new IdInvalidException("Resume doesn't exist");
+        }
+    }
+
+    @DeleteMapping("/resumes/{id}")
+    public ResponseEntity<Void> deleteResume(@PathVariable("id") String id) throws IdInvalidException {
+        if (Pattern.compile("^[0-9]+$").matcher(id).matches()) {
+            Resume currentResume = this.resumeService.handleGetResumeById(Long.parseLong(id));
+
+            if (currentResume != null) {
+                this.resumeService.handleDeleteResume(Long.parseLong(id));
+                return ResponseEntity.ok().body(null);
+            } else {
+                throw new IdInvalidException("Resume doesn't exist");
+            }
+        } else {
+            throw new IdInvalidException("Id is number");
+        }
+    }
+
+    @GetMapping("/resumes/{id}")
+    public ResponseEntity<ResResume> getResumeById(@PathVariable("id") String id) throws IdInvalidException {
+        if (Pattern.compile("^[0-9]+$").matcher(id).matches()) {
+            Resume currentResume = this.resumeService.handleGetResumeById(Long.parseLong(id));
+
+            if (currentResume != null) {
+                return ResponseEntity.ok().body(this.resumeService.handleGetResume(currentResume));
+            } else {
+                throw new IdInvalidException("Resume doesn't exist");
+            }
+        } else {
+            throw new IdInvalidException("Id is number");
+        }
+    }
+
+    @GetMapping("/resumes")
+    public ResponseEntity<ResultPaginationDTO> getAllResumes(@Filter Specification<Resume> spec, Pageable pageable){
+        return ResponseEntity.ok().body(this.resumeService.handleGetAllResumes(spec, pageable));
+    }
+}
---
+++ src/main/java/vn/hoidanit/jobhunter/service/ResumeService.java
@@ -0,0 +1,119 @@
+package vn.hoidanit.jobhunter.service;
+
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+
+import vn.hoidanit.jobhunter.domain.Job;
+import vn.hoidanit.jobhunter.domain.Resume;
+import vn.hoidanit.jobhunter.domain.User;
+import vn.hoidanit.jobhunter.domain.response.ResultPaginationDTO;
+import vn.hoidanit.jobhunter.domain.response.resume.ResCreateResumeDTO;
+import vn.hoidanit.jobhunter.domain.response.resume.ResResume;
+import vn.hoidanit.jobhunter.domain.response.resume.ResUpdateResumeDTO;
+import vn.hoidanit.jobhunter.repository.JobRepository;
+import vn.hoidanit.jobhunter.repository.ResumeRepository;
+import vn.hoidanit.jobhunter.repository.UserRepository;
+
+@Service
+public class ResumeService {
+    private ResumeRepository resumeRepository;
+    private UserRepository userRepository;
+    private JobRepository jobRepository;
+
+    public ResumeService(ResumeRepository resumeRepository, UserRepository userRepository,
+            JobRepository jobRepository) {
+        this.resumeRepository = resumeRepository;
+        this.userRepository = userRepository;
+        this.jobRepository = jobRepository;
+    }
+
+    public Resume handleGetResumeById(long id) {
+        Optional<Resume> resume = this.resumeRepository.findById(id);
+        return resume.isPresent() ? resume.get() : null;
+    }
+
+    public boolean checkResumeExistByUserAndJob(Resume resume) {
+        if (resume.getUser() == null) {
+            return false;
+        }
+        Optional<User> user = this.userRepository.findById(resume.getUser().getId());
+        if (user.isEmpty()) {
+            return false;
+        }
+
+        if (resume.getJob() == null) {
+            return false;
+        }
+        Optional<Job> job = this.jobRepository.findById(resume.getJob().getId());
+        if (job.isEmpty()) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public ResCreateResumeDTO handleCreateResume(Resume resume) {
+        resume = this.resumeRepository.save(resume);
+
+        ResCreateResumeDTO resCreateResumeDTO = new ResCreateResumeDTO();
+        resCreateResumeDTO.setId(resume.getId());
+        resCreateResumeDTO.setCreatedAt(resume.getCreatedAt());
+        resCreateResumeDTO.setCreatedBy(resume.getCreatedBy());
+
+        return resCreateResumeDTO;
+    }
+
+    public ResUpdateResumeDTO handleUpdateResume(Resume resume) {
+        resume = this.resumeRepository.save(resume);
+
+        ResUpdateResumeDTO resUpdateResumeDTO = new ResUpdateResumeDTO();
+        resUpdateResumeDTO.setUpdatedAt(resume.getUpdatedAt());
+        resUpdateResumeDTO.setUpdatedBy(resume.getUpdatedBy());
+
+        return resUpdateResumeDTO;
+    }
+
+    public void handleDeleteResume(long id) {
+        this.resumeRepository.deleteById(id);
+    }
+
+    public ResResume handleGetResume(Resume resume) {
+        ResResume resResume = new ResResume();
+
+        resResume.setId(resume.getId());
+        resResume.setEmail(resume.getEmail());
+        resResume.setUrl(resume.getUrl());
+        resResume.setStatus(resume.getStatus());
+        resResume.setCreatedAt(resume.getCreatedAt());
+        resResume.setUpdatedAt(resume.getUpdatedAt());
+        resResume.setCreatedBy(resume.getCreatedBy());
+        resResume.setUpdatedBy(resume.getUpdatedBy());
+
+        resResume.setUser(new ResResume.UserResume(resume.getUser().getId(), resume.getUser().getName()));
+        resResume.setJob(new ResResume.JobResume(resume.getJob().getId(), resume.getJob().getName()));
+
+        return resResume;
+    }
+
+    public ResultPaginationDTO handleGetAllResumes(Specification<Resume> spec, Pageable pageable) {
+        Page<Resume> pages = this.resumeRepository.findAll(spec, pageable);
+        ResultPaginationDTO resultPaginationDTO = new ResultPaginationDTO();
+        ResultPaginationDTO.Meta meta = new ResultPaginationDTO.Meta();
+
+        meta.setCurrentPage(pageable.getPageNumber() + 1);
+        meta.setPageSize(pageable.getPageSize());
+        meta.setPages(pages.getTotalPages());
+        meta.setTotal(pages.getTotalElements());
+
+        resultPaginationDTO.setMeta(meta);
+        resultPaginationDTO.setResult(
+                pages.getContent().stream().map(item -> this.handleGetResume(item)).collect(Collectors.toList()));
+
+        return resultPaginationDTO;
+    }
+}
