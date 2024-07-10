c1d487a54a2c3ef7d49dac88e38f848bcc150020
MinhDat1312 <nguyenthangdat84@gmail.com>
2024-07-10 21:45:11 +0700

    send email with template part 3

--- src/main/java/vn/hoidanit/jobhunter/controller/EmailController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/EmailController.java
@@ -5,14 +5,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import vn.hoidanit.jobhunter.service.EmailService;
+import vn.hoidanit.jobhunter.service.SubscriberService;
 
 @RestController
 @RequestMapping("/api/v1")
 public class EmailController {
     private EmailService emailService;
+    private SubscriberService subscriberService;
 
-    public EmailController(EmailService emailService) {
+    public EmailController(EmailService emailService, SubscriberService subscriberService) {
         this.emailService = emailService;
+        this.subscriberService = subscriberService;
     }
 
     @GetMapping("/email")
@@ -23,7 +26,7 @@ public class EmailController {
         // with template",
         // "<h1><b>MinhDat</b></h1>", false, true);
 
-        this.emailService.sendEmailFromTemplateSync("nguyenthangdat84@gmail.com", "Send email with template", "job");
+        this.subscriberService.sendSubscribersEmailJobs();
         return "ok";
     }
 }
--- src/main/java/vn/hoidanit/jobhunter/service/EmailService.java
+++ src/main/java/vn/hoidanit/jobhunter/service/EmailService.java
@@ -1,35 +1,30 @@
 package vn.hoidanit.jobhunter.service;
 
 import java.nio.charset.StandardCharsets;
-import java.util.List;
 
 import org.springframework.mail.MailException;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
-import vn.hoidanit.jobhunter.domain.Job;
-import vn.hoidanit.jobhunter.repository.JobRepository;
 
 @Service
 public class EmailService {
     private MailSender mailSender;
     private JavaMailSender javaMailSender;
     private SpringTemplateEngine templateEngine;
-    private JobRepository jobRepository;
 
-    public EmailService(MailSender mailSender, JavaMailSender javaMailSender, SpringTemplateEngine templateEngine,
-            JobRepository jobRepository) {
+    public EmailService(MailSender mailSender, JavaMailSender javaMailSender, SpringTemplateEngine templateEngine) {
         this.mailSender = mailSender;
         this.javaMailSender = javaMailSender;
         this.templateEngine = templateEngine;
-        this.jobRepository = jobRepository;
     }
 
     public void handleSendEmail() {
@@ -54,14 +49,12 @@ public class EmailService {
         }
     }
 
-    public void sendEmailFromTemplateSync(String to, String subject, String templateName) {
+    @Async
+    public void sendEmailFromTemplateSync(String to, String subject, String templateName, String username,
+            Object object) {
         Context context = new Context();
-
-        String name="Danny";
-        context.setVariable("name", name);
-
-        List<Job> jobs=this.jobRepository.findAll();
-        context.setVariable("jobs", jobs);
+        context.setVariable("name", username);
+        context.setVariable("jobs", object);
 
         String content = this.templateEngine.process(templateName, context);
         this.sendEmailSync(to, subject, content, false, true);
